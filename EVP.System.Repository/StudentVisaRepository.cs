9653981663d1243c6b520063246717f7fc3d8598
ny-tamang <tamangnuyang24@gmail.com>
2022-08-17 16:57:21 +0545

    Reopitories basic details complete for all models and service create

--- EVP.System.Repository/StudentVisaRepository.cs
+++ EVP.System.Repository/StudentVisaRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EVP.System.Data;
+using EVP.System.Models;
+using EVP.System.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,72 @@ using System.Threading.Tasks;
 
 namespace EVP.System.Repository
 {
-    internal class StudentVisaRepository
+    public interface IStudentVisaRepository
     {
+
+    }
+    public class StudentVisaRepository
+    {
+        private readonly ApplicationDbContext db;
+        public StudentVisaRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<StudentVisa> GetAll()
+        {
+            return db.StudentVisas;
+        }
+
+        public StudentVisa GetByID(int id)
+        {
+            var existing = db.StudentVisas.Find(id);
+            if ((existing == null) || existing.IsDeleted)
+                return null;
+            return existing;
+
+        }
+
+        public (bool, string) Create(StudentVisa model)
+        {
+            try
+            {
+                db.StudentVisas.Add(model);
+                db.SaveChanges();
+
+                return (true, "Added Successfully");
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+        public (bool, string) Edit(StudentVisa model)
+        {
+            try
+            {
+                db.StudentVisas.Update(model);
+                db.SaveChanges();
+                return (true, "It is updated successfully");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+        public (bool, string) Delete(StudentVisa model)
+        {
+            try
+            {
+                db.StudentVisas.Remove(model);
+                db.SaveChanges();
+                return (true, "It is deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
     }
 }
--- EVP.System.Repository/TouristVisaRepository.cs
+++ EVP.System.Repository/TouristVisaRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EVP.System.Data;
+using EVP.System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,68 @@ using System.Threading.Tasks;
 
 namespace EVP.System.Repository
 {
-    internal class TouristVisaRepository
+    public class TouristVisaRepository
     {
+        private readonly ApplicationDbContext db;
+        public TouristVisaRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<TouristVisa> GetAll()
+        {
+            return db.TouristVisas;
+        }
+
+        public TouristVisa GetByID(int id)
+        {
+            var existing = db.TouristVisas.Find(id);
+            if ((existing == null) || existing.IsDeleted)
+                return null;
+            return existing;
+
+        }
+
+        public (bool, string) Create(TouristVisa model)
+        {
+            try
+            {
+                db.TouristVisas.Add(model);
+                db.SaveChanges();
+
+                return (true, "Added Successfully");
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+        public (bool, string) Edit(TouristVisa model)
+        {
+            try
+            {
+                db.TouristVisas.Update(model);
+                db.SaveChanges();
+                return (true, "It is updated successfully");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+        public (bool, string) Delete(TouristVisa model)
+        {
+            try
+            {
+                db.TouristVisas.Remove(model);
+                db.SaveChanges();
+                return (true, "It is deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
     }
 }
